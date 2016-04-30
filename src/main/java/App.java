53f3941fa32f16c519d7cc53315fad6bc0ab3af2
Jonathan Klaiber <joni.klaiber@gmx.de>
2016-04-30 02:22:01 +0200

    THE ROAD IS THE GOAL
    basic init.

---
+++ src/main/java/App.java
@@ -0,0 +1,38 @@
+/**
+ * Created by JOHNY on 30.04.2016.
+ */
+
+//TODO DO STUFF!
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.tmatesoft.sqljet.core.SqlJetException;
+import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
+import org.tmatesoft.sqljet.core.table.SqlJetDb;
+
+import java.io.File;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws SqlJetException {
+
+        String DB_NAME = "DATABASE.sqlite";
+
+        File dbFile = new File(DB_NAME);
+        dbFile.delete();
+
+        SqlJetDb db = SqlJetDb.open(dbFile, true);
+        db.getOptions().setAutovacuum(true);
+        db.beginTransaction(SqlJetTransactionMode.WRITE);
+        try {
+             db.getOptions().setUserVersion(1);
+            } finally {
+             db.commit();
+            }
+    }
+}
---
+++ src/main/java/ViewController.java
@@ -0,0 +1,5 @@
+/**
+ * Created by JOHNY on 30.04.2016.
+ */
+public class ViewController {
+}
