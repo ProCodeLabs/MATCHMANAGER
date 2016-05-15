d2727cc89c5085d37efde0e6288d989a1ff4347a
Jonathan Klaiber <joni.klaiber@gmx.de>
2016-05-15 21:11:25 +0200

    fix startup bug. added resouces folder.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,16 +8,23 @@
 //TODO last commit today xD
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.tmatesoft.sqljet.core.SqlJetException;
 import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
 import org.tmatesoft.sqljet.core.table.SqlJetDb;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 //Test
 
 public class App extends Application {
+
+    Logger log = Logger.getLogger(this.getClass().getName());
 
     public static void main(String[] args) {
         launch(args);
@@ -26,6 +33,20 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws SqlJetException {
 
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/layout.fxml"));
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (Exception ex) {
+            log.info("Error in start: "+ex);
+        }
+
+        databaseSetup();
+
+    }
+
+    public void databaseSetup() throws SqlJetException {
         String DB_NAME = "DATABASE.sqlite";
 
         File dbFile = new File(DB_NAME);
@@ -35,9 +56,9 @@ public class App extends Application {
         db.getOptions().setAutovacuum(true);
         db.beginTransaction(SqlJetTransactionMode.WRITE);
         try {
-             db.getOptions().setUserVersion(1);
-            } finally {
-             db.commit();
-            }
+            db.getOptions().setUserVersion(1);
+        } finally {
+            db.commit();
+        }
     }
 }
