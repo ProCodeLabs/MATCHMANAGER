89c795f02c21a309c3adce2c81654e8c92caf93b
clemi <clemi295@googlemail.com>
2016-05-25 12:13:46 +0200

    updated formatting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,43 +22,55 @@ import java.util.logging.Logger;
 
 //Test
 
-public class App extends Application {
+public class App extends Application
+{
 
-    Logger log = Logger.getLogger(this.getClass().getName());
+	Logger log = Logger.getLogger( this.getClass( ).getName( ) );
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main( String[] args )
+	{
 
-    @Override
-    public void start(Stage primaryStage) throws SqlJetException {
+		launch( args );
+	}
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("fxml/layout.fxml"));
-            primaryStage.initStyle(StageStyle.UNDECORATED);
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-        } catch (Exception ex) {
-            log.info("Error in start: "+ex);
-        }
+	@Override
+	public void start( Stage primaryStage ) throws SqlJetException
+	{
 
-        databaseSetup();
+		try
+		{
+			Parent root = FXMLLoader.load( getClass( ).getResource( "fxml/layout.fxml" ) );
+			primaryStage.initStyle( StageStyle.UNDECORATED );
+			primaryStage.setScene( new Scene( root ) );
+			primaryStage.show( );
+		}
+		catch( Exception ex )
+		{
+			log.info( "Error in start: " + ex );
+		}
 
-    }
+		databaseSetup( );
 
-    public void databaseSetup() throws SqlJetException {
-        String DB_NAME = "DATABASE.sqlite";
+	}
 
-        File dbFile = new File(DB_NAME);
-        dbFile.delete();
+	public void databaseSetup( ) throws SqlJetException
+	{
+		String DB_NAME = "DATABASE.sqlite";
 
-        SqlJetDb db = SqlJetDb.open(dbFile, true);
-        db.getOptions().setAutovacuum(true);
-        db.beginTransaction(SqlJetTransactionMode.WRITE);
-        try {
-            db.getOptions().setUserVersion(1);
-        } finally {
-            db.commit();
-        }
-    }
+		File dbFile = new File( DB_NAME );
+		dbFile.delete( );
+
+		SqlJetDb db = SqlJetDb.open( dbFile, true );
+		db.getOptions( ).setAutovacuum( true );
+		db.beginTransaction( SqlJetTransactionMode.WRITE );
+
+		try
+		{
+			db.getOptions( ).setUserVersion( 1 );
+		}
+		finally
+		{
+			db.commit( );
+		}
+	}
 }
