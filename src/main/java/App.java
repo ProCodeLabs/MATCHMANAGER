5fab2657745501c206e932765b89d787f50f78de
clemi <clemi295@googlemail.com>
2016-05-25 13:08:30 +0200

    dirty implemented undecorated window movement added packages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,9 +8,12 @@
 //TODO last commit today xD
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.tmatesoft.sqljet.core.SqlJetException;
@@ -20,28 +23,78 @@ import org.tmatesoft.sqljet.core.table.SqlJetDb;
 import java.io.File;
 import java.util.logging.Logger;
 
-//Test
 
 public class App extends Application
 {
 
 	Logger log = Logger.getLogger( this.getClass( ).getName( ) );
 
+	private static double xOffset = 0;
+	private static double yOffset = 0;
+
+	private static double BORDER_HEIGHT = 18;
+	private static boolean isDragged = false;
+
+
 	public static void main( String[] args )
 	{
-
 		launch( args );
 	}
 
 	@Override
-	public void start( Stage primaryStage ) throws SqlJetException
+	public void start( final Stage primaryStage ) throws SqlJetException
 	{
 
 		try
 		{
 			Parent root = FXMLLoader.load( getClass( ).getResource( "fxml/layout.fxml" ) );
 			primaryStage.initStyle( StageStyle.UNDECORATED );
-			primaryStage.setScene( new Scene( root ) );
+
+			Scene scene = new Scene( root );
+
+
+			scene.addEventFilter( MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>( )
+			{
+				public void handle( MouseEvent event )
+				{
+					if( event.getSceneY( ) <= BORDER_HEIGHT )
+					{
+						xOffset = event.getSceneX( );
+						yOffset = event.getSceneY( );
+
+						//primaryStage.setX( event.getScreenX( ) - event.getSceneX( ) );
+						//primaryStage.setY( event.getScreenY( ) - event.getSceneY( ) );
+
+						isDragged = true;
+						event.consume( );
+					}
+				}
+			} );
+
+			scene.addEventFilter( MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>( )
+			{
+				public void handle( MouseEvent event )
+				{
+					if( isDragged )
+					{
+						primaryStage.setX( event.getScreenX( ) - xOffset  );
+						primaryStage.setY( event.getScreenY( ) - yOffset );
+
+						event.consume( );
+					}
+				}
+			} );
+
+			scene.addEventFilter( MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>( )
+			{
+				public void handle( MouseEvent event )
+				{
+					isDragged = false;
+				}
+			} );
+
+
+			primaryStage.setScene( scene );
 			primaryStage.show( );
 		}
 		catch( Exception ex )
--- src/main/java/ViewController.java
+++ src/main/java/ViewController.java
@@ -1,21 +1,15 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * Created by JOHNY on 30.04.2016.
- */
-public class ViewController implements Initializable
+
+public class ViewController
 {
-
-	public void initialize( URL location, ResourceBundle resources )
-	{
-
-	}
 
 	@FXML
 	Button closeButton;
@@ -26,4 +20,9 @@ public class ViewController implements Initializable
 		Stage stage = ( Stage ) closeButton.getScene( ).getWindow( );
 		stage.close( );
 	}
+
+
+
+
+
 }
