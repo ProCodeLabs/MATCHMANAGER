24643a03e16e917383b3ca81a0e2b593f1510988
Jonathan Klaiber <joni.klaiber@gmx.de>
2016-06-03 18:34:35 +0200

    added ListView of Data sources small fixes

--- src/main/java/Common/Files.java
+++ src/main/java/Common/Files.java
@@ -2,9 +2,6 @@ package Common;
 
 import java.io.File;
 
-/**
- * Created by JOHNY on 03.06.2016.
- */
 public class Files
 {
 	public static final String path = System.getProperty( "user.home" ) + File.separator + "Matchmanager";
