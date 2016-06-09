1c4ae86ca8d65315ea71d2f0570fdd831e42e634
Jonathan Klaiber <joni.klaiber@gmx.de>
2016-06-09 17:23:02 +0200

    style fixes

--- src/main/java/ui/Controller/SelectDatabaseController.java
+++ src/main/java/ui/Controller/SelectDatabaseController.java
@@ -9,6 +9,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import ui.Helper.UiBaseContainer;
 
 import java.io.File;
 import java.net.URL;
@@ -18,114 +19,105 @@ import java.util.logging.Logger;
 
 import static Common.Files.PATH;
 
-public class SelectDatabaseController implements Initializable
-{
-	public static final String RESOURCE_ID = "fxml/centerContent/selectDatabase.fxml";
+public class SelectDatabaseController implements Initializable {
+    public static final String RESOURCE_ID = "fxml/centerContent/selectDatabase.fxml";
 
-	final Logger log = Logger.getLogger( this.getClass( ).getName( ) );
+    final Logger log = Logger.getLogger(this.getClass().getName());
 
-	ObservableList<String> _fileList = FXCollections.observableArrayList( );
+    ObservableList<String> _fileList = FXCollections.observableArrayList();
 
-	@FXML
-	Button newButton;
+    @FXML
+    Button newButton;
 
-	@FXML
-	Button loadButton;
+    @FXML
+    Button loadButton;
 
-	@FXML
-	Button deleteButton;
+    @FXML
+    Button deleteButton;
 
-	@FXML
-	ListView<String> dataList;
-
-
-	@Override
-	public void initialize( URL location, ResourceBundle resources )
-	{
-		TaskManager.runUiTask( ( ) -> scanDataFolder( _fileList ) );
-		dataList.setItems( _fileList );
-	}
+    @FXML
+    ListView<String> dataList;
 
 
-
-	@FXML
-	public void buttonLoadClicked( )
-	{
-		//TODO: Load selected data
-		String datapath = getSelectedPath( );
-		System.out.println( "Load clicked" );
-	}
-
-	@FXML
-	public void buttonNewClicked( )
-	{
-		TextInputDialog dialog = new TextInputDialog( "" );
-		dialog.setTitle( "Create Database" );
-		dialog.setHeaderText( "Create new Database" );
-		dialog.setContentText( "Please enter a Database name:" );
-		Optional<String> result = dialog.showAndWait( );
-		result.ifPresent( name -> {
-			DatabaseHandler.createNewDatabase( name );
-			_fileList.add( name );
-		} );
-	}
-
-	@FXML
-	public void buttonDeleteClicked( )
-	{
-		Alert alert = new Alert( Alert.AlertType.CONFIRMATION );
-		alert.setTitle( "Dalete" );
-		alert.setHeaderText( "Delete" );
-		alert.setContentText( "Are you sure you want to Delete this?" );
-
-		Optional<ButtonType> result = alert.showAndWait( );
-		if( result.get( ) == ButtonType.OK )
-		{
-			DatabaseHandler.deleteDatabase( getSelectedPath( ) );
-			_fileList.remove( getSelected( ) );
-		}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        TaskManager.runUiTask(() -> scanDataFolder(_fileList));
+        dataList.setItems(_fileList);
+    }
 
 
-	}
+    @FXML
+    public void buttonLoadClicked() {
+        //TODO: Load selected data
+        String datapath = getSelectedPath();
+
+        UiBaseContainer container = (UiBaseContainer) loadButton.getScene().getRoot();
+        {
+            try
+            {
+                container.setCenter("OVERVIEW", MainViewController.RESOURCE_ID);
+            } catch (Exception ex) {
+
+            }
+
+        }
+    }
+
+    @FXML
+    public void buttonNewClicked() {
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Create Database");
+        dialog.setHeaderText("Create new Database");
+        dialog.setContentText("Please enter a Database name:");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(name -> {
+            DatabaseHandler.createNewDatabase(name);
+            _fileList.add(name);
+        });
+    }
+
+    @FXML
+    public void buttonDeleteClicked() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Dalete");
+        alert.setHeaderText("Delete");
+        alert.setContentText("Are you sure you want to Delete this?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            DatabaseHandler.deleteDatabase(getSelectedPath());
+            _fileList.remove(getSelected());
+        }
 
 
+    }
 
 
-	public void scanDataFolder( ObservableList<String> data )
-	{
-		String path = System.getProperty( "user.home" ) + File.separator + "Matchmanager";
-		File f = new File( path );
-		if( !f.exists( ) )
-		{
-			f.mkdir( ) ;
-		}
+    public void scanDataFolder(ObservableList<String> data) {
+        String path = System.getProperty("user.home") + File.separator + "Matchmanager";
+        File f = new File(path);
+        if (!f.exists()) {
+            f.mkdir();
+        }
 
-		try
-		{
-			for( File i : f.listFiles( ) )
-			{
-				if( i.getName().lastIndexOf( ".sqlite" ) > 0 )
-				{
-					String s = i.getName( ).replace( ".sqlite", "" );
-					Platform.runLater( ( ) -> data.add( s ) );
-				}
-			}
-		}
-		catch( NullPointerException e )
-		{
-			log.log( LogLevel.ERROR, "File is null! " + e.getMessage( ) );
-		}
-	}
+        try {
+            for (File i : f.listFiles()) {
+                if (i.getName().lastIndexOf(".sqlite") > 0) {
+                    String s = i.getName().replace(".sqlite", "");
+                    Platform.runLater(() -> data.add(s));
+                }
+            }
+        } catch (NullPointerException e) {
+            log.log(LogLevel.ERROR, "File is null! " + e.getMessage());
+        }
+    }
 
 
+    public String getSelected() {
+        return dataList.getSelectionModel().getSelectedItem();
+    }
 
-	public String getSelected( )
-	{
-		return dataList.getSelectionModel( ).getSelectedItem( );
-	}
-
-	public String getSelectedPath( )
-	{
-		return PATH + File.separator + getSelected( );
-	}
+    public String getSelectedPath() {
+        return PATH + File.separator + getSelected();
+    }
 }
